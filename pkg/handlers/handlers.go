45429f32337ed1cf79cde4def3010a311f612cfa
gaurondorian@gmail.com <gaurondorian@gmail.com>
2023-08-10 18:41:42 +0200

    :sparkles: init

---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,89 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+	"github.com/michelprogram/serverless-go/pkg/user"
+)
+
+var ErrorMethodNotAllowed = "method not allowed"
+
+type ErrorBody struct {
+	ErrorMsg *string `json:"error,omitempty"`
+}
+
+func GetUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI) (*events.APIGatewayProxyResponse, error) {
+
+	email := req.QueryStringParameters["email"]
+
+	if len(email) > 0 {
+		res, err := user.FetchUser(email, tableName, dynaClient)
+
+		if err != nil {
+			return apiResponse(http.StatusBadRequest, ErrorBody{
+				aws.String(err.Error()),
+			})
+		}
+
+		return apiResponse(http.StatusOK, res)
+	}
+
+	res, err := user.FetchUsers(tableName, dynaClient)
+
+	if err != nil {
+		return apiResponse(http.StatusBadRequest, ErrorBody{
+			aws.String(err.Error()),
+		})
+	}
+
+	return apiResponse(http.StatusOK, res)
+
+}
+
+func CreateUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI) (*events.APIGatewayProxyResponse, error) {
+
+	res, err := user.CreateUser(req, tableName, dynaClient)
+
+	if err != nil {
+		return apiResponse(http.StatusBadRequest, ErrorBody{
+			aws.String(err.Error()),
+		})
+	}
+
+	return apiResponse(http.StatusCreated, res)
+
+}
+
+func UpdateUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI) (*events.APIGatewayProxyResponse, error) {
+
+	res, err := user.UpdateUser(req, tableName, dynaClient)
+
+	if err != nil {
+		return apiResponse(http.StatusBadRequest, ErrorBody{
+			aws.String(err.Error()),
+		})
+	}
+
+	return apiResponse(http.StatusOK, res)
+
+}
+
+func DeleteUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI) (*events.APIGatewayProxyResponse, error) {
+
+	err := user.DeleteUser(req, tableName, dynaClient)
+
+	if err != nil {
+		return apiResponse(http.StatusBadRequest, ErrorBody{
+			aws.String(err.Error()),
+		})
+	}
+
+	return apiResponse(http.StatusOK, nil)
+}
+
+func UnhandledMethod() (*events.APIGatewayProxyResponse, error) {
+	return apiResponse(http.StatusMethodNotAllowed, ErrorMethodNotAllowed)
+}
